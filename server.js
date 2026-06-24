739ba70ea3449d91b5834e60ff8944de302e17bb
hassanbinilyas <hassanbinilyas@outlook.com>
2026-06-24 15:48:07 +0500

    *

--- server.js
+++ server.js
@@ -2,41 +2,38 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const usersRouter = require("./routes/api/users");
-const path = require("path");
-require("dotenv").config(); 
-
+const config = require('config');
 const app = express();
-
 // Body parser middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
 app.use(express.json());
-
 // DB Config
-const db = process.env.MONGO_URI;
-
+const db = config.get('mongoURI');
 // Connect to MongoDB
 mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-
+    .connect(db)
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
 // Passport middleware
 app.use(passport.initialize());
-
 // Passport config
 require("./config/passport")(passport);
-
 // Routes
 app.use("/api/users", usersRouter);
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+/* //Serve static assets if in production
+if (process.env.NODE_ENV = "production") {
+    app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
+ */
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
